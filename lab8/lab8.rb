9d45916bf9be11a82cd2260718c442abccae04be
Adam J Penn <apenn1@ecc-jaguar.csuchico.edu>
2016-03-16 18:36:20 -0700

    Almost deleted lab8.rb, got it mostly working just need to pull only files with name not whole dir

--- lab8/lab8.rb
+++ lab8/lab8.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/env ruby
 
 # make sure that there are 3 args passed in
-if ARGV.length != 1
+if ARGV.length != 3
   puts "Incorrect Number of Arguments"
   puts "Correct Usage: full_backup.sh <keyword> <directory_name> <tarfile_name>"
 end
 
-matchingfiles = `find .`.split
-matchingfiles.keep_if { |x| x =~ /.*#{ARGV[0]}.*/ }
+matchingFiles = Array.new
+Dir.chdir("#{ARGV[1]}") do
+  matchingFiles = `find .`.split
+  matchingFiles.keep_if { |file| file =~ /.*#{ARGV[0]}.*/ }
+end
 
-puts matchingfiles
+filesToTar = ""
+matchingFiles.each do |file|
+  filesToTar << (ARGV[1] + "/" + file.delete("./") + " ")
+end
+
+`tar -cvf #{ARGV[2]} #{filesToTar} 2>/dev/null`
 
